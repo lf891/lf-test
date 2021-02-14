fbc24e9d200c9dcb7c37d0e9aedb568180276542
lf <lf>
2021-02-14 16:39:47 -0300

    Improves component preview

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,114 @@
 <template>
   <div id="app">
-    <div class="container">
+    <div class="lf-container">
       <h2>LfDropdown</h2>
-      <h3>Default</h3>
-      <lf-dropdown
-        label="Botão"
-        variant="danger"
-      >
-        <lf-dropdown-item href="https://contaazul.com">Link</lf-dropdown-item>
-        <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
-      </lf-dropdown>
+      <h3>default</h3>
+      <div class="lf-grid">
+        <div
+          class="lf-grid__col"
+          v-for="variant in dropdownVariants"
+          :key="variant"
+        >
+          <lf-dropdown
+            :label="capitalize(variant)"
+            :variant="variant"
+          >
+            <lf-dropdown-item href="https://www.contaazul.com/">https://www.contaazul.com/</lf-dropdown-item>
+            <lf-dropdown-item href="https://www.contaazul.com/" new-tab>https://www.contaazul.com/(New Tab)</lf-dropdown-item>
+            <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
+          </lf-dropdown>
+        </div>
+      </div>
 
-      <h3>Split</h3>
-      <lf-dropdown
-        label="Botão"
-        variant="danger"
-        split
-      >
-        <lf-dropdown-item href="https://contaazul.com">Link</lf-dropdown-item>
-        <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
-      </lf-dropdown>
+      <hr class="lf-divider">
+
+      <h3>split prop</h3>
+      <div class="lf-grid">
+        <div
+          class="lf-grid__col"
+          v-for="variant in dropdownVariants"
+          :key="variant"
+        >
+          <lf-dropdown
+            label="Split"
+            :variant="variant"
+            split
+            @click="mainButtonClick"
+          >
+            <lf-dropdown-item href="https://www.contaazul.com/">https://www.contaazul.com/</lf-dropdown-item>
+            <lf-dropdown-item href="https://www.contaazul.com/" new-tab>https://www.contaazul.com/(New Tab)</lf-dropdown-item>
+            <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
+          </lf-dropdown>
+        </div>
+      </div>
+
+      <hr class="lf-divider">
+
+      <h3>close-on-click="false" prop</h3>
+      <div class="lf-grid">
+        <div
+          class="lf-grid__col"
+          v-for="variant in dropdownVariants"
+          :key="variant"
+        >
+          <lf-dropdown
+            :label="capitalize(variant)"
+            :variant="variant"
+            :close-on-click="false"
+            split
+          >
+            <lf-dropdown-item href="https://www.contaazul.com/">https://www.contaazul.com/</lf-dropdown-item>
+            <lf-dropdown-item href="https://www.contaazul.com/" new-tab>https://www.contaazul.com/(New Tab)</lf-dropdown-item>
+            <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
+          </lf-dropdown>
+        </div>
+      </div>
+
+      <hr class="lf-divider">
+
+      <h3>close-on-click-outside="false" prop</h3>
+      <div class="lf-grid">
+        <div
+          class="lf-grid__col"
+          v-for="variant in dropdownVariants"
+          :key="variant"
+        >
+          <lf-dropdown
+            :label="capitalize(variant)"
+            :variant="variant"
+            :close-on-click-outside="false"
+            split
+          >
+            <lf-dropdown-item href="https://www.contaazul.com/">https://www.contaazul.com/</lf-dropdown-item>
+            <lf-dropdown-item href="https://www.contaazul.com/" new-tab>https://www.contaazul.com/(New Tab)</lf-dropdown-item>
+            <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
+          </lf-dropdown>
+        </div>
+      </div>
+
+      <hr class="lf-divider">
+
+      <h3>disabled prop</h3>
+      <div class="lf-grid">
+        <div
+          class="lf-grid__col"
+          v-for="variant in dropdownVariants"
+          :key="variant"
+        >
+          <lf-dropdown
+            label="Split"
+            disabled
+            :variant="variant"
+            :close-on-click="false"
+            split
+            @click="mainButtonClick"
+          >
+            <lf-dropdown-item href="https://www.contaazul.com/">https://www.contaazul.com/</lf-dropdown-item>
+            <lf-dropdown-item href="https://www.contaazul.com/" new-tab>https://www.contaazul.com/(New Tab)</lf-dropdown-item>
+            <lf-dropdown-item @click="dropdownItemClick">dropdownItemClick()</lf-dropdown-item>
+          </lf-dropdown>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,23 +122,31 @@ export default {
     LfDropdown,
     LfDropdownItem,
   },
+  data() {
+    return {
+      dropdownVariants: ['default', 'primary', 'secondary', 'danger'],
+    };
+  },
   methods: {
+    capitalize(value) {
+      if (!value) {
+        return '';
+      }
+
+      return value.toString().charAt(0).toUpperCase() + value.slice(1);
+    },
+    mainButtonClick() {
+      // eslint-disable-next-line
+      alert('Main Button Click');
+    },
     dropdownItemClick() {
       // eslint-disable-next-line
-      alert('Dropdown Item was clicked');
+      alert('Dropdown Item Click');
     },
   },
 };
 </script>
 
 <style lang="scss">
-body,
-html {
-  height: 100%;
-}
-
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-}
+@import './assets/styles/index.scss';
 </style>
